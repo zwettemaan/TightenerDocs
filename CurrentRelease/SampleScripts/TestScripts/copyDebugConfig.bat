109173b950dfce9c7ba5cb8acba4e8528a3f9969
Kris Coppieters <kris@rorohiko.com>
2022-11-29 20:47:14 +1300

    Splitting release scripts from binary code

--- CurrentRelease/SampleScripts/TestScripts/copyDebugConfig.bat
+++ CurrentRelease/SampleScripts/TestScripts/copyDebugConfig.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-IF "%TIGHTENER_GIT_ROOT%" == "" GOTO ERROR_MISSING_RELEASE_ROOT
-IF NOT EXIST "%TIGHTENER_GIT_ROOT%" GOTO ERROR_NON_EXISTENT_RELEASE_ROOT
+IF "%TIGHTENER_RELEASE_ROOT%" == "" GOTO ERROR_MISSING_RELEASE_ROOT
+IF NOT EXIST "%TIGHTENER_RELEASE_ROOT%" GOTO ERROR_NON_EXISTENT_RELEASE_ROOT
 
 IF NOT EXIST "%APPDATA%\net.tightener" MKDIR "%APPDATA%\net.tightener"
 IF NOT EXIST "%APPDATA%\net.tightener\SysConfig" MKDIR "%APPDATA%\net.tightener\SysConfig"
@@ -16,13 +16,13 @@ GOTO :DONE
 
 :ERROR_NON_EXISTENT_RELEASE_ROOT
 
-ECHO Directory %%TIGHTENER_GIT_ROOT%% does not exist. Please follow instructions in README.md
+ECHO Directory %%TIGHTENER_RELEASE_ROOT%% does not exist. Please follow instructions in README.md
 
 GOTO DONE
 
 :ERROR_MISSING_RELEASE_ROOT
 
-ECHO %%TIGHTENER_GIT_ROOT%% has not been set up. Please follow instructions in README.md
+ECHO %%TIGHTENER_RELEASE_ROOT%% has not been set up. Please follow instructions in README.md
 
 GOTO DONE
 
