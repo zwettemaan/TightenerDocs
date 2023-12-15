f01cc37c8972627f8c18bdeeb4934ab548a577e6
Kris Coppieters <kris@rorohiko.com>
2023-12-15 18:54:44 +1300

    Test code

--- CurrentRelease/Plug-Ins/TightenerESDLL/sampleCode.jsx
+++ CurrentRelease/Plug-Ins/TightenerESDLL/sampleCode.jsx
@@ -4,4 +4,13 @@ TIGHTENER.init();
 
 TIGHTENER.lib.tghInit('ESDLLSampleCoordinator');
 
-alert(TIGHTENER.lib.licGetMachineGUID());
+alert(TIGHTENER.lib.getMachineGUID());
+
+var e = TIGHTENER.lib.encryptStr("Hello World", "mykey");
+alert(e);
+
+var d = TIGHTENER.lib.decryptStr(e, "mykey");
+alert(d);
+
+var c = TIGHTENER.lib.getCapability("c09f0226c1b21909b7415341c1d55a07", "p2", "password");
+alert(">" + c + "<");
