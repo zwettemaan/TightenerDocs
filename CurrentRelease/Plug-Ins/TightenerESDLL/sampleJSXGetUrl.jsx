c4b0dd3663f49313885fd2b6109230c1f8179ca5
Kris Coppieters <kris@rorohiko.com>
2023-12-27 22:21:28 +1300

    Sync from Linux

---
+++ CurrentRelease/Plug-Ins/TightenerESDLL/sampleJSXGetUrl.jsx
@@ -0,0 +1,23 @@
+
+//@include "lib/TightenerESDLLLoader.jsx"
+var getURL = JSXGetURL();
+
+alert(getURL.isJSXGetURLDemo());
+
+getURL.addRequestHeader("Accept: */*");
+var s = getURL.get("https://www.rorohiko.com") + "";
+
+alert(s.substr(0,1000));
+
+var headers = getURL.getResponseHeaders();
+alert(headers);
+
+// Some random FTP file for testing
+
+var f = new File("~/Desktop/sha512.sum");
+var s = getURL.get("ftp://cygwin.com/pub/gcc/sha512.sum", f.fsName);
+
+// Some zip file to test binary file download
+
+var f = new File("~/Desktop/FrameReporter.1.1.8.zip");
+getURL.get("https://www.rorohiko.com/downloads/FrameReporter.1.1.8.zip", f.fsName);
---
+++ CurrentRelease/Plug-Ins/TightenerESDLL/sampleTightenerCode.jsx
@@ -0,0 +1,16 @@
+﻿//@include "lib/TightenerESDLLLoader.jsx"
+
+TIGHTENER.init();
+
+TIGHTENER.lib.tghInit('ESDLLSampleCoordinator');
+
+alert("Machine GUID = " + TIGHTENER.lib.getMachineGUID());
+
+var e = TIGHTENER.lib.encryptStr("Hello World", "mykey");
+alert("Encrypted string = " + e);
+
+var d = TIGHTENER.lib.decryptStr(e, "mykey");
+alert("Decrypted string = " + d);
+
+//var c = TIGHTENER.lib.getCapability("c09f0226c1b21909b7415341c1d55a07", "code", "password");
+//alert(">" + c + "<");
